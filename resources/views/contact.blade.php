b971312b969b2c2f3acfe2befb39fcc14c61b11b
priskaadr <priska4523085@univpancasila.ac.id>
2025-10-15 14:52:34 +0700

    pertemuan 3

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Informasi Kontak - LaraPress</title>
+</head>
+<body>
+   <h1>Hubungi Kami</h1>
+   <p>Silakan hubungi kami melalui informasi berikut:</p>
+
+   <ul>
+      <li><strong>Alamat:</strong> Jl. Lulut,Pojok Muara, 1610 Klapanunggal Bogor</li>
+      <li><strong>Email:</strong> priskaadr@gmail.com</li>
+      <li><strong>Telepon:</strong> (+62) 85181017175</li>
+      <li><strong>Instagram:</strong> <a href="https://instagram.com/larapress" target="_blank">@larapress</a></li>
+   </ul>
+
+   <a href="/">Kembali ke Halaman Utama</a>
+</body>
+</html>
